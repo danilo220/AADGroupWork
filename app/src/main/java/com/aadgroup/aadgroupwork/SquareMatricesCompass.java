d6826a177ced2bd68fabb46dec255449a53473f4
Danilo Ribeiro <dan18nucks@gmail.com>
2018-01-30 23:50:04 +0000

    first commit, adding all classes

---
+++ app/src/main/java/com/aadgroup/aadgroupwork/SquareMatricesCompass.java
@@ -0,0 +1,18 @@
+package com.aadgroup.aadgroupwork;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Danilo on 30/01/2018.
+ */
+
+public class SquareMatricesCompass extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.square_matrices_compass);
+    }
+}
