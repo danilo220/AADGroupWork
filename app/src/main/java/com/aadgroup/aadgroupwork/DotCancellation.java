43d21654cf7474c91ce84c3ea6b3cae334f0c8e0
NTU-ASH <ashley.belshaw2015@my.ntu.ac.uk>
2018-02-09 13:45:57 +0000

    Added images and Tile Class
    Basic additions

--- app/src/main/java/com/aadgroup/aadgroupwork/DotCancellation.java
+++ app/src/main/java/com/aadgroup/aadgroupwork/DotCancellation.java
@@ -3,10 +3,6 @@ package com.aadgroup.aadgroupwork;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- * Created by Danilo on 30/01/2018.
- */
-
 public class DotCancellation extends AppCompatActivity
 {
     @Override
---
+++ app/src/main/java/com/aadgroup/aadgroupwork/Tile.java
@@ -0,0 +1,22 @@
+package com.aadgroup.aadgroupwork;
+
+public class Tile {
+    private int numberOfDots;
+    private Boolean isActive = false;
+    private int imageName;
+
+    public int getNumberOfDots()
+    {
+        return numberOfDots;
+    }
+
+    public Boolean getIsActive()
+    {
+        return isActive;
+    }
+
+    public int getImageName()
+    {
+        return imageName;
+    }
+}
