1932310e4674a3387aa312444fbcdb4f86e97459
NTU-ASH <ashley.belshaw2015@my.ntu.ac.uk>
2018-02-12 20:46:11 +0000

    Language Selection And Misc
    Added language selection for English or German
    Added blank results activity for clinicians to view
    Added scroll views for Dot Cancellation and some other changes

--- app/src/main/java/com/aadgroup/aadgroupwork/MainActivity.java
+++ app/src/main/java/com/aadgroup/aadgroupwork/MainActivity.java
@@ -1,15 +1,23 @@
 package com.aadgroup.aadgroupwork;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
 import static java.lang.System.in;
 
@@ -17,6 +25,7 @@ public class MainActivity extends AppCompatActivity
 {
     ArrayList<Account> allAccounts = new ArrayList<>();
     int accIndex = -1;
+    private boolean userIsInteracting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,7 +79,7 @@ public class MainActivity extends AppCompatActivity
                     loggedOnLayout.setVisibility(View.VISIBLE);
 
                     TextView loggedInMsg = findViewById(R.id.txtLoggedInMsg);
-                    loggedInMsg.setText("You are logged in as " + username + "!");
+                    loggedInMsg.setText(getResources().getString(R.string.loggedIn) + username + "!");
 
                     if (allAccounts.get(accIndex).getIsClinician())
                     {
@@ -116,6 +125,75 @@ public class MainActivity extends AppCompatActivity
                 startActivity(myIntent);
             }
         });
+
+        Button resultsButton = findViewById(R.id.btn_results);
+        resultsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(getApplicationContext(), Results.class);
+                startActivity(myIntent);
+            }
+        });
+
+        //http://www.theappguruz.com/blog/multi-language-support-to-android-app
+        final Spinner languageSelect = findViewById(R.id.spn_language);
+        languageSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                if (userIsInteracting) {
+
+                    String selectedLanguage = languageSelect.getSelectedItem().toString();
+
+                    switch (selectedLanguage) {
+                        case "English":
+                            switchLanguage("en");
+                            //this.setContentView(R.layout.activity_main);
+                            Toast toast = Toast.makeText(getApplicationContext(), "en", Toast.LENGTH_SHORT);
+                            toast.show();
+                            break;
+                        case "German":
+                            switchLanguage("de");
+                            Toast toast2 = Toast.makeText(getApplicationContext(), "de", Toast.LENGTH_SHORT);
+                            toast2.show();
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+            }
+
+            public void switchLanguage(String languageCode)
+            {
+                /*String languageToLoad = languageCode; // your language
+                Locale locale = new Locale(languageToLoad);
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                this.setContentView(R.layout.activity_main)*/
+                Locale myLocale = new Locale(languageCode);
+                Resources res = getResources();
+                DisplayMetrics dm = res.getDisplayMetrics();
+                Configuration conf = res.getConfiguration();
+                conf.locale = myLocale;
+                res.updateConfiguration(conf, dm);
+                Intent refresh = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(refresh);
+                finish();
+            }
+
+
+        });
+    }
+    @Override
+    public void onUserInteraction()
+    {
+        super.onUserInteraction();
+        userIsInteracting = true;
     }
 
 }
